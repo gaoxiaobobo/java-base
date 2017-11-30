e33e3716ffefd23ae25f1218730954d5fedf0b04
gaoxiaobo <gaoxiaobobo@qq.com>
2017-11-30 08:52:31 +0800

    sort lambda and ignore

---
+++ src/main/java/com/xiaobo/jdk/lambda/Person.java
@@ -0,0 +1,31 @@
+package com.xiaobo.jdk.lambda;
+
+public class Person {
+    private  String name;
+    private  int age;
+
+    public int getAge() { return age; }
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/xiaobo/jdk/lambda/SortPeople.java
@@ -0,0 +1,55 @@
+package com.xiaobo.jdk.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+
+/**
+ * 参考网址
+ * https://www.mkyong.com/java8/java-8-foreach-examples/
+ */
+public class SortPeople {
+    public static void main(String[] args) {
+        Person[] people = {new Person("gxb", 33),
+                new Person("zxl", 34),
+                new Person("xy", 32)};
+//        sort(people);
+//        sortDes(people);
+
+        List<Person> l = asList(new Person("holi",12),
+                new Person("paopao",7),
+                new Person("bb",3));
+        List<Person> filterL = l.stream().filter(p-> p.getAge()>3).collect(Collectors.toList());
+        l.forEach(p-> System.out.println(p));
+
+        filterL.forEach(System.out::println);
+
+
+    }
+    static void sort(Person[] people){
+        Comparator<Person> byName = Comparator.comparing(p -> p.getName());
+        Arrays.sort(people, byName);
+        for (Person p : people) {
+            System.out.println(p);
+        }
+        System.out.println("--------");
+
+//        方法引用
+        Comparator<Person> byAge = Comparator.comparing(Person::getAge);
+        Arrays.sort(people,byAge);
+
+        asList(people).forEach(System.out::println);
+    }
+
+    static void sortDes(Person[] people){
+        System.out.println("desc --------");
+        Comparator<Person> byName = Comparator.comparing(Person::getName).reversed();
+        Arrays.sort(people,byName);
+        asList(people).forEach(item-> System.out.println(item));
+
+    }
+
+}
